90084e99efddeecfc17a2d18ce16d4f9767405e4
faisalsayed10 <faisal.sayed502@gmail.com>
2021-06-02 18:38:12 +0530

    Change theme color

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Flutter Calculator",
-      theme: ThemeData(primarySwatch: Colors.red),
+      theme: ThemeData(primarySwatch: Colors.blue),
       home: HomePage(),
     );
   }
